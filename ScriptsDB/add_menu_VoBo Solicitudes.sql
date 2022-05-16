129b7fb57df67008406245164a80a71f3de9e2e3
Pedro Castillo <agonzalez@itechdev.com.mx>
2022-05-16 14:50:25 -0500

    Se añaden paginas al menu

---
+++ ScriptsDB/add_menu_VoBo Solicitudes.sql
@@ -0,0 +1,41 @@
+
+DECLARE @idMenu int =0
+		,@descripcion nvarchar(40) = ' VoBo Solicitudes'
+		,@padre int =3
+		,@posicion int =0
+		,@habilitada bit =1 
+		,@url nvarchar(50) ='../Solicitudes/SolicitudVobo.aspx'
+
+select @idMenu= max(idMenu)+1  from tbl_Menu
+
+select @posicion = MAX(posicion)+1 from tbl_Menu where PadreId=3
+
+
+
+INSERT INTO [dbo].[tbl_Menu]
+           ([Descripcion]
+           ,[PadreId]
+           ,[Posicion]
+           ,[Habilitada]
+           ,[Url]
+           ,[P1]
+           ,[P2]
+           ,[P3]
+           ,[P4]
+           ,[P5]
+           ,[P6]
+           ,[P7])
+     VALUES
+           (@descripcion
+           ,@padre
+           ,@posicion
+           ,@habilitada
+           ,@url
+           ,1
+           ,0
+           ,0
+           ,0
+           ,0
+           ,0
+           ,0)
+GO
