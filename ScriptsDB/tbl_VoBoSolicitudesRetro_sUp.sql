768632b8d619d05a4e8c74faf19ba92f64d4c95c
Pedro Castillo <agonzalez@itechdev.com.mx>
2022-06-09 19:51:09 -0500

    Actualizacion de SCRIPT

--- ScriptsDB/tbl_VoBoSolicitudesRetro_sUp.sql
+++ ScriptsDB/tbl_VoBoSolicitudesRetro_sUp.sql
@@ -1,47 +1,30 @@
 IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id('dbo.tbl_VoBoSolicitudesRetro_sUp') and sysstat & 0xf = 4)
 DROP PROCEDURE dbo.tbl_VoBoSolicitudesRetro_sUp
 GO
--- =============================================
--- Author:		Pedro Castillo
--- Create date: 13-05-2022,
--- Description: crea los registros para la retroalimentacion de las solicitudes,
--- =============================================
-CREATE PROCEDURE dbo.tbl_VoBoSolicitudesRetro_sUp
-	-- Add the parameters for the stored procedure here
-		@Id_voBoSol int
-		,@correo varchar(50)		           
-		,@id_user int
-AS
-BEGIN
-	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
-
-    -- Insert statements for procedure here
-
-
-INSERT INTO [dbo].[tbl_VoBoSolicitudesRetro]
-           ([Id_voBoSol]
-           ,[correo]
-           ,[comentariosNegocio]
-           ,[riesgosDestacados]
-           ,[autorizado]
-           ,[fec_autorizado]
-           ,[fec_movto]
-           ,[id_user])
-     VALUES
-           (@Id_voBoSol 
-           ,@correo
-           ,''
-           ,''
-           ,null
-           ,null
-           ,GETDATE()
-           ,@id_user )
-
-
-
-END
+-- =============================================  
+-- Author:  Pedro Castillo  
+-- Create date: 13-05-2022,  
+-- Description: Consulta las retro por folio de solicitud,  
+-- =============================================  
+CREATE PROCEDURE tbl_VoBoSolicitudesRetro_sUp  
+ -- Add the parameters for the stored procedure here  
+  @Id_voBoSol int  
+    
+AS  
+BEGIN  
+ -- SET NOCOUNT ON added to prevent extra result sets from  
+ -- interfering with SELECT statements.  
+ SET NOCOUNT ON;  
+  
+    -- Insert statements for procedure here  
+  
+  
+ SELECT * FROM tbl_VoBoSolicitudesRetro  
+ WHERE Id_voBoSol=@Id_voBoSol  
+  
+  
+  
+END  
 GO
 GRANT EXECUTE ON OBJECT::dbo.tbl_VoBoSolicitudesRetro_sUp TO produccion;
 GO
